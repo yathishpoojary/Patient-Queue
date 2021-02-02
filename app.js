0bdd6f98574ea28173eedf65f17d670b90cff2d0
yathishpoojary <yathishpoojary023@gmail.com>
2021-02-02 13:59:59 +0530

    removed unused file and logs

--- app.js
+++ app.js
@@ -1,242 +1,222 @@
 
 patientList = [];
+const baseURL = "http://localhost:8080/"
 
 initialize();
 
 // insering the user values into table
-function insertRowToTable(patient)
-{
+function insertRowToTable(patient) {
 
-  var table=document.getElementById("myTable");
+  var table = document.getElementById("myTable");
 
-    var row=table.insertRow();
-    var cell1=row.insertCell(0);
-    var cell2=row.insertCell(1);
-    var cell3=row.insertCell(2);
-    var cell4=row.insertCell(3);
-    var cell5= row.insertCell(4);
+  var row = table.insertRow();
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  var cell6 = row.insertCell(5);
 
-    cell1.innerHTML = patient.pName;
-    cell2.innerHTML = patient.pAddress;
-    cell3.innerHTML =patient.pNumber;
-   cell4.innerHTML = patient.pProblem;
-   cell5.innerHTML=  '<button  type="button" onClick="deleteFunction(this)" >'
-   + 'REMOVE</button>'//"Delete";  <span class="glyphicon glyphicon-pencil"></span>   //  <button> delete </button>   ///  '<button class=\"btn btn-primary btn-xs my-xs-btn\" type='button' onClick='changeRec(".$num.")'></button>';
-   //console.log("table", table, patient)
+  cell1.innerHTML = patient.pName;
+  cell2.innerHTML = patient.pAddress;
+  cell3.innerHTML = patient.pNumber;
+  cell4.innerHTML = patient.pProblem;
+  cell5.innerHTML = '<button  type="button" onClick="deleteFunction(this)" >'
+    + 'REMOVE</button>'
 
-} 
+}
 
 // user submitted data
-function submitPatientData(){
+function submitPatientData() {
 
- // createDatabase();
-    var name=document.getElementById('name').value;
-    var number=document.getElementById('number').value;
-    var address=document.getElementById('address').value;
-    var problem=document.getElementById('problem').value;
+  // createDatabase();
+  var name = document.getElementById('name').value;
+  var number = document.getElementById('number').value;
+  var address = document.getElementById('address').value;
+  var problem = document.getElementById('problem').value;
 
-    // validate user entered data
-    if(name=="" || name==undefined){
-      alert("Please enter name");
-    }else if(number=="" || number==undefined){
-      alert("Please enter number");
-    }else if(address=="" || address==undefined){
-      alert("Please enter address");
-    }else if(problem=="" || problem==undefined || problem == "Choose"){
-      alert("Please select problem");
-    }else{
+  // validate user entered data
+  if (name == "" || name == undefined) {
+    alert("Please enter name");
+  } else if (number == "" || number == undefined) {
+    alert("Please enter number");
+  } else if (address == "" || address == undefined) {
+    alert("Please enter address");
+  } else if (problem == "" || problem == undefined || problem == "Choose") {
+    alert("Please select problem");
+  } else {
 
-      // if data is correct, create Object of the data 
-      var patient = new Object();
-      patient.pName =name;
-      patient.pAddress = address;
-      patient.pNumber = number;
-      patient.pProblem = problem;
+    // if data is correct, create Object of the data 
+    var patient = new Object();
+    patient.pName = name;
+    patient.pAddress = address;
+    patient.pNumber = number;
+    patient.pProblem = problem;
 
-      submitpatient(patient);
+    submitpatient(patient);
 
-    }
-}
-  
-function search() {
-  var     cell;
- var input = document.getElementById("myInput");
- var  convertUpper = input.value.toUpperCase();
- const table = document.getElementById("myTable");
-   var tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-  var  td = tr[i].getElementsByTagName("td");
-    for(j=0;j< td.length;j++){
-      if(j==0||j==2||j==3){
-    var   textValue = td[j].innerText;
-      if (textValue) {
-        if (textValue.toUpperCase().indexOf(convertUpper) > -1) 
-        {
-          tr[i].style.display = "";
-          break;
-        } else {
-          tr[i].style.display = "none";
-        }
-      } 
-    }  
-    }
-        
   }
 }
 
-function deleteFunction(obj)
-{
-  var id =  obj.parentElement.parentElement.rowIndex;
- // document.getElementById("myTable").deleteRow(id);
-  console.log("delete id is " +id);
-  console.log("patientList.length is "+patientList.length);
-  for(var i=0;i<patientList.length;i++){
-    if((id-1) == i){
-    //  patientList.splice(i,1);
-      console.log("  === ");
-      console.log("patient list is "+patientList[i]);
+function search() {
+  var cell;
+  var input = document.getElementById("myInput");
+  var inputUpper = input.value.toUpperCase();
+  const table = document.getElementById("myTable");
+  var tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    var td = tr[i].getElementsByTagName("td");
+    for (j = 0; j < td.length; j++) {
+      if (j == 0 || j == 2 || j == 3) {
+        var textValue = td[j].innerText;
+        if (textValue) {
+          console.log("in search window " + (textValue.toUpperCase().indexOf(inputUpper)));
+          if (textValue.toUpperCase().indexOf(inputUpper) > -1) {
+            tr[i].style.display = "";
+            break;
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+    }
+
+  }
+}
+
+function deleteFunction(obj) {
+  var id = obj.parentElement.parentElement.rowIndex;
+  for (var i = 0; i < patientList.length; i++) {
+    if ((id - 1) == i) {
+      //  patientList.splice(i,1);
       deletePatientData(patientList[i]);
     }
   }
 
- // setPatientListToLocalStorage();
+  // setPatientListToLocalStorage();
 }
 
-function createDatabase(){
-  try{
-   if(window.openDatabase){
-           var shortName   =  'db_edentiti';
-           var version   =  '1.0';
-           var displayName  =  'Edentiti Information';
-           var maxSize   =  65536; // in bytes
-           db    =  openDatabase(shortName, version, displayName, maxSize);
-                  alert('Sqlite Database created');
-       }
-  }catch(e){
-   alert(e);
+function createDatabase() {
+  try {
+    if (window.openDatabase) {
+      var shortName = 'db_edentiti';
+      var version = '1.0';
+      var displayName = 'Edentiti Information';
+      var maxSize = 65536; // in bytes
+      db = openDatabase(shortName, version, displayName, maxSize);
+      alert('Sqlite Database created');
+    }
+  } catch (e) {
+    alert(e);
   }
 }
-   
-function setPatientListToLocalStorage(){
+
+function setPatientListToLocalStorage() {
   localStorage.setItem('PatientList', JSON.stringify(patientList));
 }
 
-function getPatientListFromLocalStorage(){
+function getPatientListFromLocalStorage() {
   const data = JSON.parse(localStorage.getItem('PatientList'));
-  if(data){
+  if (data) {
     patientList = data;
-  }else{
+  } else {
     patientList = [];
   }
 }
 
-function removePatientListFromLocalStorage(){
+function removePatientListFromLocalStorage() {
   localStorage.removeItem('PatientList');
   patientList = [];
 }
 
-function removePatientList(){
+function removePatientList() {
 
-  for(i=patientList.length;i>0;i--){
-    document.getElementById("myTable").deleteRow(i);  
-  }
-  removePatientListFromLocalStorage();
+  // for (i = patientList.length; i > 0; i--) {
+  //   document.getElementById("myTable").deleteRow(i);
+  // }
+  // removePatientListFromLocalStorage();
+
+  deleteAllPatientData();
+}
+
+function initialize() {
+  // getPatientListFromLocalStorage();
+  //  if(patientList){
+  //   for(var i=0;i<patientList.length;i++){
+  //     insertRowToTable(patientList[i]);
+  //   }
+  // }
+
+  getPatientData();
+
 
 }
 
-function initialize(){
- // getPatientListFromLocalStorage();
-//  if(patientList){
-//   for(var i=0;i<patientList.length;i++){
-//     insertRowToTable(patientList[i]);
-//   }
-// }
-
- getPatientData();
-
- 
-}  
-
-function getPatientData(){
-  console.log("curser entered to the script tag ...");
-
+function getPatientData() {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-   if (this.readyState == 4 && this.status == 200) {
-         // document.getElementById("demo").innerHTML = this.responseText;
-         console.log("this.responseText", this.responseText)
-        // alert("successfully submitted")
-
-        for(i=patientList.length;i>0;i--){
-          document.getElementById("myTable").deleteRow(i);  
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      for (i = patientList.length; i > 0; i--) {
+        document.getElementById("myTable").deleteRow(i);
+      }
+      patientList = JSON.parse(this.responseText);
+      if (patientList) {
+        for (var i = 0; i < patientList.length; i++) {
+          insertRowToTable(patientList[i]);
         }
-
-        patientList =  JSON.parse(this.responseText);
-        console.log("nsjfnkjsafn", patientList);
-
-        if(patientList){
-          for(var i=0;i<patientList.length;i++){
-            insertRowToTable(patientList[i]);
-          }
-        }
-   }
+      }
+    }
   };
-  xhttp.open("GET"," http://localhost:8080/getPatients",true );
+  xhttp.open("GET", baseURL + "getPatients", true);
   xhttp.send();
-} 
+}
 
-function submitpatient(patient){
-
+function submitpatient(patient) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-   if (this.readyState == 4 && this.status == 200) {
-         // document.getElementById("demo").innerHTML = this.responseText;
-         alert(this.responseText);
-
-         if(patientList){
-          // push into array
-          patientList.push(patient);
-        }
-        insertRowToTable(patient);
-        setPatientListToLocalStorage();   
-   }
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+      if (patientList) {
+        patientList.push(patient);
+      }
+      insertRowToTable(patient);
+      setPatientListToLocalStorage();
+    }
   };
-  xhttp.open("POST"," http://localhost:8080/submitPatientData",true );
-  xhttp.setRequestHeader("Content-type","application/json");
-  var data = JSON.stringify(patient); 
+  xhttp.open("POST", baseURL + "submitPatientData", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  var data = JSON.stringify(patient);
   xhttp.send(data);
-}  
+}
 
 
-function deletePatientData(patient){
-    console.log("curser comes to delete patient data ......"+patient);
-     
+function deletePatientData(patient) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-   if (this.readyState == 4 && this.status == 200) {
-         // document.getElementById("demo").innerHTML = this.responseText;
-         console.log("this.responseText", this.responseText);
-         getPatientData();
-
-        // alert("successfully submitted")
-
-        // patientList =  JSON.parse(this.responseText);
-        // console.log("nsjfnkjsafn", patientList);
-        // if(patientList){
-        //   for(var i=0;i<patientList.length;i++){
-        //     insertRowToTable(patientList[i]);
-        //   }
-        // }
-   }
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+      getPatientData();
+    }
   };
 
-  xhttp.open("POST"," http://localhost:8080/removePatients",true );
-  xhttp.setRequestHeader("Content-type","application/json");
-  var data = JSON.stringify(patient); 
+  xhttp.open("POST", baseURL + "removePatients", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  var data = JSON.stringify(patient);
   console.log(data);
   xhttp.send(data);
+}
 
-  // xhttp.open("DELETE"," http://localhost:8080/removePatients",true );
-  // var data = JSON.stringify(patient); 
-  // xhttp.send(data);
+function deleteAllPatientData() {
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+      getPatientData();
+    }
+  };
+
+  xhttp.open("POST", baseURL + "removeAllPatients", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send();
 } 
--- server.js
+++ server.js
@@ -2,92 +2,66 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors');
 const app = express();
-const MongoClient =require('mongodb').MongoClient;
-const connectionString="mongodb+srv://root:dinga@patientqueuecluster.7xnvk.mongodb.net/PatientQueue?retryWrites=true&w=majority";
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = "mongodb+srv://root:dinga@patientqueuecluster.7xnvk.mongodb.net/PatientQueue?retryWrites=true&w=majority";
+
 app.use(cors());
-
-
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-MongoClient.connect(connectionString,(err,client)=> {
-    if (err) return console.error(err);
+MongoClient.connect(connectionString, (err, client) => {
+  if (err) return console.error(err);
 
-    console.log('Connected to Database');
-    const db=client.db('PatientQueue');
-    const patientCollection = db.collection('Patients');
+  console.log('Connected to Database');
+  const db = client.db('PatientQueue');
+  const patientCollection = db.collection('Patients');
 
-    app.post('/submitPatientData', (req, res) => {
-        console.log("req", req.body);
-    
-        patientCollection.insertOne(req.body)
-        .then(result => {
-          console.log("result", result)
-          res.send(`Successfully ** submitted ${req.body.pName} patient data`);
-        })
-        .catch(error =>{
-            console.error(error);
-        })
-        
-    });
-
-    
-    app.get('/getPatients', (req, res) => {
-        db.collection('Patients').find().toArray()
-          .then(results => {
-            console.log("results array", results);
-            res.send(results);
-          })
-          .catch(error => console.error(error))
-        // ...
+  app.post('/submitPatientData', (req, res) => {
+    patientCollection.insertOne(req.body)
+      .then(result => {
+        res.send(`Successfully submitted ${req.body.pName} patient data`);
       })
-       
-
-      app.post('/removePatients', (req, res) => {
-        console.log("remove patient method")
-        console.log("delete patient request object is ", req.body);
-
-        var myquery = { pName: req.body.pName };
-      //  var myquery = { _id: req.body._id };
-        patientCollection.deleteOne(myquery, function(err, obj) {
-        if (err) throw err;
-        console.log("1 document deleted", obj);
-        res.send(obj);
-        //db.close();
+      .catch(error => {
+        console.error(error);
+      })
   });
 
-        //   patientCollection.deleteOne( { name: req.body.pName })
-        //   .then(result => {
-        //     if (result.deletedCount === 0) {
-        //       return res.json('No quote to delete')
-        //     }
-        //     res.json(`Deleted `+ req.body.pName)
-        //   })
-        //   .catch(error => console.error(error))
+  app.get('/getPatients', (req, res) => {
+    db.collection('Patients').find().toArray()
+      .then(results => {
+        res.send(results);
       })
-       
-    
+      .catch(error => console.error(error))
   })
 
+  app.post('/removePatients', (req, res) => {
+    var myquery = { pNumber: req.body.pNumber };
+    patientCollection.deleteOne(myquery, function (err, obj) {
+      if (err) throw err;
+      console.log("1 document deleted", obj.deletedCount);
+      if (obj.deletedCount == 0) {
+        res.send("No Patient to delete");
+      } else {
+        res.send("Deleted successfully");
+      }
+    });
+  })
 
-app.post('/example', (req, res) => {
-    res.send(`Full name is:${req.query.fname} ${req.body.lname}.`);
-  });
-  
-  app.get('/getPatientList', (req, res) => {
-    res.send([{"name":"Suma"},{"name":"Yathish"}]);
-  });
-  
-//   app.post('/submitPatientData', (req, res) => {
-//       console.log("req", req.body);
-  
-      
-//       res.send(`Successfully submitted ${req.body.pName} patient data`);
-//   });
-  
-  const port = 8080;
-  
-  app.listen(port, () => {
-    console.log(`Server running on port${port}`);
-  });
-  
+  app.post('/removeAllPatients', (req, res) => {
+    patientCollection.deleteMany({}, function (err, obj) {
+      if (err) throw err;
+      console.log("1 document deleted", obj.deletedCount);
+      if (obj.deletedCount == 0) {
+        res.send("No Patient to delete ");
+      } else {
+        res.send("Deleted successfully");
+      }
+    });
+  })
+
+})
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server running on port${port}`);
+});
